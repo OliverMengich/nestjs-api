12295ba3d32955ae8836391f1ae89b3b095fd011
OliverMengich <kemeioliver2000@gmail.com>
2023-07-12 13:19:51 +0300

    Token addition and user service

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,13 +7,19 @@ import { PrismaService } from './prisma.service';
 import { LocationService } from './Location/location.service';
 import { SpeakerService } from './Speaker/speaker.service';
 import { AuthService } from './auth/auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { UserService } from './Users/user.service';
 // import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: ['.env'],
     }),
-    // AuthModule,
+    JwtModule.register({
+      secret: 'secret',
+      signOptions: { expiresIn: '1d' },
+      global: true,
+    }),
   ],
   controllers: [AppController],
   providers: [
@@ -23,6 +29,7 @@ import { AuthService } from './auth/auth.service';
     LocationService,
     SpeakerService,
     AuthService,
+    UserService,
   ],
 })
 export class AppModule {}
