e9967e8354b7cacd2737f26b7185db917fefcdd2
OliverMengich <kemeioliver2000@gmail.com>
2023-07-20 16:15:09 +0300

    DB name and ttodo

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,19 @@ import { AppModule } from './app.module';
 import * as express from 'express';
 import { join } from 'path';
 // QR cide scan, feedback, certificate generation and emailing, agenda https://www.youtube.com/watch?v=jD9DLU2_kUE
+// =======Event App Todo====
+// - Document generation PASS
+// - Image upload
+// - Event registration form
+// - Maps integration
+// - State management integration
+// - Notifications integration
+// - Checkout page integration
+// - Login as admin or User or Speaker
+// 	- Checkout for user if not speaker or user
+// - State management and data accessing from the cache
+// - Event registration form
+// - Events history
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['verbose'],
